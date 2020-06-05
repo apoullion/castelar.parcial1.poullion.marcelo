d6a4f2bf49aa3a2c491de1acf8e509b7d369e767
Marcelo Poullion <marcelo.poullion@alumnos.uai.edu.ar>
2020-06-04 21:17:12 -0300

    segundo commit

---
+++ Parcial1.Poullion/Parcial1.Poullion/Models/Constelacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1.Poullion.Models
+{
+    public class Constelacion
+    {
+
+        public String Siglo { get; set; }
+        public String Nombre { get; set; }
+    }
+}
---
+++ Parcial1.Poullion/Parcial1.Poullion/Models/GestorConstelacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1.Poullion.Models
+{
+    public class GestorConstelacion
+    {
+        private static List<Constelacion> constelaciones = new List<Constelacion>();
+
+        public static void InitLista()
+        {
+            constelaciones.Add(new Constelacion { Siglo = "19", Nombre="Prueba" });
+
+        }
+
+        public static List<Constelacion> GetConstelaciones()
+        {
+            return constelaciones;
+
+        }
+
+        public static void Add(Constelacion constelacion )
+        {
+            constelaciones.Add(constelacion);
+        }
+    }
+}
